1cf58ea235dce0654cb480645c5a5256fb6d5f91
tommyy1708 <hzhan081@fiu.edu>
2024-02-07 23:23:57 -0500

    (1)Modify the component for csvUpload can show the name of uploaded file

--- src/Components/CsvUpload/CsvUpload.jsx
+++ src/Components/CsvUpload/CsvUpload.jsx
@@ -6,17 +6,19 @@ const CsvUpload = () => {
   const [csvFile, setCsvFile] = useState('');
   const [isDisable, setIsDisable] = useState(true);
   const customRequest = async ({ file, onSuccess, onError }) => {
-    await setCsvFile(file);
-    if (onSuccess) {
-      message.info('upload success');
-      setIsDisable(false);
-    } else {
-      console.log(onError);
-    }
+  console.log("🚀 ~ customRequest ~ file:", file)
+
+    // await setCsvFile(file);
+    // if (onSuccess) {
+    //   message.info('upload success');
+    //   setIsDisable(false);
+    // } else {
+    //   console.log(onError);
+    // }
   };
 
   const submit_csv = async () => {
-    const response = await UploadCsv(csvFile);
+    const response = await UploadCsv(fileList[0]);
     if (response.status === 'success') {
       message.success('Success');
     }
@@ -28,6 +30,40 @@ const CsvUpload = () => {
     return true;
   };
 
+  //!!
+  const [fileList, setFileList] = useState([
+    // {
+    //   uid: '-1',
+    //   name: 'xxx.png',
+    //   status: 'done',
+    //   url: 'http://www.baidu.com/xxx.png',
+    // },
+  ]);
+  const handleChange = (info) => {
+    console.log('🚀 ~ handleChange ~ info:', info);
+    let newFileList = [...info.fileList];
+
+    // 1. Limit the number of uploaded files
+    // Only to show two recent uploaded files, and old ones will be replaced by the new
+    newFileList = newFileList.slice(-1);
+
+    // 2. Read from response and show file link
+    newFileList = newFileList.map((file) => {
+      if (file.response) {
+        // Component will show file.url as link
+        file.url = file.response.url;
+      }
+      return file;
+    });
+    setFileList(newFileList);
+
+    setIsDisable(false);
+  };
+  const props = {
+    action: 'http://127.0.0.1:8000/api/upload-csv',
+    onChange: handleChange,
+    multiple: true,
+  };
   return (
     <Space
       direction="vertical"
@@ -38,9 +74,10 @@ const CsvUpload = () => {
     >
       <Upload
         listType="text"
-        customRequest={customRequest}
+        {...props}
+        fileList={fileList}
+        // customRequest={customRequest}
         beforeUpload={beforeUpload}
-        showUploadList={false}
       >
         <Button icon={<UploadOutlined />}>Upload</Button>
       </Upload>
